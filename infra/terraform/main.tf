59a467a39771dc4db93796d2f704e6764bc9aefa
Amit Kushwaha <amitkushwaha0102@gmail.com>
2026-05-05 11:40:22 +0000

    remove use_lockfile from backend config

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,7 +10,6 @@ terraform {
     bucket         = "devops-accelerator-tf-state-amit2700"
     key            = "capstone/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile = true
     encrypt        = true
   }
 }
